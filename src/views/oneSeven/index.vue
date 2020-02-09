93932dd704544ff1b5e41fdd8773b91e0c07a48f
GeGeWu123 <2404498384@qq.com>
2020-02-09 12:07:29 +0800

    init

---
+++ src/views/oneSeven/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="ml30">一七级课程体系</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.ml30{
+  margin-left: 30px;
+}
+</style>
---
+++ src/views/video/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- <video class="m50" width="1000" height="" controls="controls" autoplay="autoplay">
+      <source src="../../assets/信管专业介绍.mp4" type="video/mp4" />
+    </video> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.ml30{
+  margin-left: 30px;
+}
+.m50{
+  margin: 50px;
+}
+</style>
